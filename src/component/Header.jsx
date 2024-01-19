f3a51b11c98a7534420a9df50f2aac88023fdeac
Arnab Ghosh <ajg616725@gmail.com>
2024-01-19 12:13:52 +0530

    ryuytu67y

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./Props.css";
 
-import logo from "../assets/logo.png"
-import { RiShieldUserLine,RiMenuSearchLine } from "react-icons/ri";
+import logo from "../assets/logo.png";
+import { RiShieldUserLine, RiMenuSearchLine } from "react-icons/ri";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { GiPerfumeBottle } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 const Props = () => {
-
   return (
     <>
       <nav className="section__container nav__container">
         <a href="/" className="nav__logo">
-          <img height={'100px'} src={logo}></img>
+          <img height={"100px"} src={logo}></img>
         </a>
         <ul className="nav__links">
           <li className="link">
-            <Link  to={'/home'}>HOME</Link>
+            <Link to={"/home"}>HOME</Link>
           </li>
           <li className="link">
             <a href="/products">SHOP</a>
@@ -28,20 +27,27 @@ const Props = () => {
         </ul>
         <div className="nav__icons">
           <span>
-            <RiShieldUserLine />
+            <Link to={"/home"}>
+              <RiShieldUserLine />
+            </Link>
           </span>
           <span>
-            <RiMenuSearchLine />
+            <Link to={"/home"}>
+              <RiMenuSearchLine />
+            </Link>
           </span>
           <span>
-            <HiOutlineShoppingBag />
+            <Link to={"/home"}>
+              <HiOutlineShoppingBag />
+            </Link>
           </span>
           <span>
-            <GiPerfumeBottle />
+            <Link to={"/home"}>
+              <GiPerfumeBottle />
+            </Link>
           </span>
         </div>
       </nav>
- 
     </>
   );
 };
