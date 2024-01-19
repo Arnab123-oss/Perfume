7ce4e410987f6ab2438c4d4caa1b4a8c143e4872
Ankit Yadav <ankit.yadav@bassetti-group.com>
2024-01-19 12:02:01 +0530

    fix overflow

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -12,7 +12,6 @@ const ImageSlider = () => {
     'https://i.ibb.co/NSwVv8D/img3.jpg',
     'https://i.ibb.co/Bq4Q0M8/img4.jpg',
     'https://i.ibb.co/jTQfmTq/img5.jpg',
-    'https://i.ibb.co/RNkk6L0/img6.jpg',
   ]);
 
   const handlePrevClick = () => {
